23c2050265f4150e510ee552f93625a666f06f8a
tcbitt <114869382+tcbitt@users.noreply.github.com>
2023-09-23 16:14:18 -0400

    Create Prog2part2.c

---
+++ Prog2part2.c
@@ -0,0 +1,38 @@
+/*
+Name: Travis Bittner
+Email: travis.bittner@und.edu
+
+Simple C program that get user input for width, height, and a symbol to fill with.
+Uses nested while loops to print a box of size width x height and fill it with the symbol.
+*/  
+#include <stdio.h>
+
+int main(){
+    int width, height;
+    char symbol;
+    
+    printf("Enter width: ");
+    scanf("%d", &width);
+    
+    printf("Enter height: ");
+    scanf("%d", &height);
+    
+    printf("Enter symbol: ");
+    
+    //Clear input buffer to get character
+    while(getchar() != '\n');
+    symbol = getchar();
+    
+    while(height > 0) {
+        //Set this because we need use the original width as we iterate
+        int i = width;
+        
+        while(i > 0) {
+            printf("%c", symbol);
+            i -= 1;
+        }
+        printf("\n");
+        height -= 1;
+        
+    }
+}
