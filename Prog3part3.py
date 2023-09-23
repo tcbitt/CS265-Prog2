0a42520612ca1bb6a647bd917b354ea533465634
tcbitt <114869382+tcbitt@users.noreply.github.com>
2023-09-23 17:48:26 -0400

    Create Prog3part3.py

---
+++ Prog3part3.py
@@ -0,0 +1,15 @@
+# Name: Travis Bittner
+# Email: travis.bittner@und.edu
+# 
+# Simple Python program that gets 2 values from the user, a start and end.
+# Checks if start is less than end, if so loop and count up, else loop and count down
+
+start = int(input("Enter starting value: "))
+end = int(input("Enter ending value: "))
+
+if start < end:
+    for i in range(start, end + 1, 1):
+        print(i)
+else:
+    for i in range(start, end + 1, -1):
+        print(i)
